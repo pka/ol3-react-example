ae25f84fc358172eb65fc174f9a808cd4380350e
Pirmin Kalberer <pka@sourcepole.ch>
2015-08-24 09:47:55 +0200

    React/Redux boilerplate based on https://github.com/jackielii/simplest-redux-example

---
+++ README.md
@@ -0,0 +1,10 @@
+Basic example for using [OpenLayers 3](http://openlayers.org/) with [React](http://facebook.github.io/react/) and [Redux](http://rackt.github.io/redux/)
+
+To run this example:
+
+1. `npm install`
+2. `npm start`
+3. open http://localhost:3000/ in the browser
+
+GeoJSON data is from 
+http://www.geoforall.org/locations/OSGEoLabs.json
---
+++ index.js
@@ -0,0 +1,73 @@
+var React = require('react');
+var Redux = require('redux');
+var ReactRedux = require('react-redux');
+var createStore = Redux.createStore;
+var Provider = ReactRedux.Provider;
+var connect = ReactRedux.connect;
+
+// React component
+var Counter = React.createClass( {
+  render: function() {
+    var value = this.props.value;
+    var onIncreaseClick = this.props.onIncreaseClick;
+    return (
+      <div>
+        <span>{value}</span>
+        <button onClick={onIncreaseClick}>Increase</button>
+      </div>
+    );
+  }
+});
+
+// Action:
+var increaseAction = {type: 'increase'};
+
+// Reducer:
+function counter(state, action) {
+  if (typeof state === 'undefined') {
+    state = {count: 0};
+  }
+  var count = state.count;
+  switch(action.type){
+    case 'increase':
+      return {count: count+1};
+    default:
+      return state;
+  }
+}
+
+// Store:
+var store = createStore(counter);
+
+// Map Redux state to component props
+function mapStateToProps(state)  {
+  return {
+    value: state.count
+  };
+}
+
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    onIncreaseClick: function() {
+      dispatch(increaseAction)
+    }
+  };
+}
+
+// Connected Component:
+var App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+React.render(
+  React.createElement(Provider, {store: store}, 
+    function(){
+      return React.createElement(App, null)
+    }
+  ),
+  document.getElementById('root')
+);
+
+module.exports = Counter;
